12f01cc8a67df21e753d961c9129ef7b0a99fb09
HLAM <hemilioaraujo@gmail.com>
2019-08-16 08:43:31 -0300

    files upload

---
+++ organiza.py
@@ -0,0 +1,36 @@
+import os, shutil
+from pathlib import Path
+
+path = Path('.')
+suffixes = list({filepath.suffix[1:] for filepath in path.glob('*')})
+
+# print(suffixes)
+
+def cria_pastas(suffixes):
+	for suffix in suffixes:
+		if suffix != '':
+			os.mkdir(Path('.')/f'{suffix}')
+
+
+def get_itens_por_tipo(suffix):
+    itens = list(Path('.').glob(f'*.{suffix}'))
+    #print(itens)
+    return itens
+
+
+def move_itens(suffixes):
+    for suffix in suffixes:
+        if suffix != '':
+            print(get_itens_por_tipo(suffix))
+            itens = get_itens_por_tipo(suffix)
+            for item in itens:
+                src = item
+                dst = Path('.')/f'{suffix}'
+                src = os.fspath(src)
+                dst = os.fspath(dst)
+                print(f'Origem: -> {src}\nDestino: -> {dst}')
+                shutil.move(src, dst)
+
+
+cria_pastas(suffixes)
+move_itens(suffixes)
