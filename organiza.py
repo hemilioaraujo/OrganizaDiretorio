3d7f352ef6e62e427eb6e2c47928c756feb56122
HLAM <hemilioaraujo@gmail.com>
2020-01-24 14:52:29 -0300

    removida verificação de item vazio da função cria_pastas e adicionada na list comprehension do suffixes

--- organiza.py
+++ organiza.py
@@ -2,36 +2,36 @@ import os, shutil
 from pathlib import Path
 
 path = Path('.')
-suffixes = list({filepath.suffix[1:] for filepath in path.glob('*')})
+# suffixes = list({filepath.suffix[1:] for filepath in path.glob('*')})
+suffixes = [filepath.suffix[1:] for filepath in path.glob('*') if filepath.suffix[1:] != '']
 
 print(suffixes)
 print(path)
 
 def cria_pastas(suffixes):
 	for suffix in suffixes:
-		if suffix != '':
-			os.mkdir(Path('.')/f'{suffix}')
-
+		os.mkdir(Path('.')/f'{suffix}')
+		
 
 def get_itens_por_tipo(suffix):
-    itens = list(Path('.').glob(f'*.{suffix}'))
-    #print(itens)
-    return itens
+	itens = list(Path('.').glob(f'*.{suffix}'))
+	#print(itens)
+	return itens
 
 
 def move_itens(suffixes):
-    for suffix in suffixes:
-        if suffix != '':
-            print(get_itens_por_tipo(suffix))
-            itens = get_itens_por_tipo(suffix)
-            for item in itens:
-                src = item
-                dst = Path('.')/f'{suffix}'
-                src = os.fspath(src)
-                if src != 'organiza.py':
-                    dst = os.fspath(dst)
-                    print(f'Origem: -> {src}\nDestino: -> {dst}')
-                    shutil.move(src, dst)
+	for suffix in suffixes:
+		if suffix != '':
+			print(get_itens_por_tipo(suffix))
+			itens = get_itens_por_tipo(suffix)
+			for item in itens:
+				src = item
+				dst = Path('.')/f'{suffix}'
+				src = os.fspath(src)
+				if src != 'organiza.py':
+					dst = os.fspath(dst)
+					print(f'Origem: -> {src}\nDestino: -> {dst}')
+					shutil.move(src, dst)
 
 
 cria_pastas(suffixes)
